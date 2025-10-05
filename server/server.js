5b851bbc7c65a187a756b478728f14d91f14a632
shashankbhattakash <akashbhatt5227@gmail.com>
2025-10-05 21:02:49 +0530

    added deployed frontend link

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: "https://e-comerce-app-frontend.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
